b5e010ea1d0db884feaac3314998e02f52806d5e
HentekoP <78004170+HentekoP@users.noreply.github.com>
2023-07-19 14:50:01 +0900

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,13 @@
 ﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
-#include <iostream>
+#include <stdio.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(void) {
+	printf("hello world!\n");
+	return 0;
 }
+
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
