a27abcf5a4f50b28f7adf3888502282c73d2140b
HentekoP <78004170+HentekoP@users.noreply.github.com>
2023-08-09 15:03:10 +0900

    tesd

--- ConsoleApplication1/ConsoleApplication1/kakeru_waru.cpp
+++ ConsoleApplication1/ConsoleApplication1/kakeru_waru.cpp
@@ -1,22 +1,22 @@
-//
-//#include <stdio.h>
-//
-//int main(void) {
-//	int a, b;
-//	printf("Š|‚¯ZŠ„‚èZ‚ğ‚µ‚Ü‚·B”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
-//	printf("a: ");
-//	scanf_s("%d", &a);
-//	printf("b: ");
-//	scanf_s("%d", &b);
-//
-//	int sum = a * b;
-//	int difference = a / b;
-//
-//	printf("%d*%d", a, b);
-//	printf(" = %d\n", sum);
-//	printf("%d/%d", a, b);
-//	printf(" = %d\n", difference);
-//
-//	return 0;
-//}
-//
+
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+	printf("Š|‚¯ZŠ„‚èZ‚ğ‚µ‚Ü‚·B”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+	printf("a: ");
+	scanf_s("%d", &a);
+	printf("b: ");
+	scanf_s("%d", &b);
+
+	int sum = a * b;
+	int difference = a / b;
+
+	printf("%d*%d", a, b);
+	printf(" = %d\n", sum);
+	printf("%d/%d", a, b);
+	printf(" = %d\n", difference);
+
+	return 0;
+}
+
